67c077496f6a0405fafdcf60b7c723e283b409c9
NotIllia <ilya.maznichenko@businessgroup.ltd>
2024-11-16 16:44:52 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by m223 on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
